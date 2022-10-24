c1025dc572f275a4f3fdf6b6c31d97c86a6725f8
stephaniemaf <farrell.s.ava@gmail.com>
2022-10-24 10:03:51 +0000

    add code to calculate the random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+//wait for the dom to finish loading before running the game
+//get the button elements and add the listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button")
+
+    for(let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("you cloicked Submit")
+            }
+            else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You Clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame(){
 
 }
