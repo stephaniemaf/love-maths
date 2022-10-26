79c3ed6216074389f45a935747d26203c5c2b839
stephaniemaf <farrell.s.ava@gmail.com>
2022-10-26 16:33:40 +0000

    added favivon and final tidy up for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,14 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         })
     }
-
+    /**
+     * used to press enter on keyboard instead of clicking submit
+     */
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter"){
+            checkAnswer();
+        }
+    })
     runGame("addition");
 })
 /**
@@ -23,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the users first answer has been presented
  */
 function runGame(gameType){
+
+   document.getElementById("answer-box").value = "";
+   document.getElementById("answer-box").focus();
    let num1 =  Math.floor(Math.random() * 25) + 1;
    let num2 =  Math.floor(Math.random() * 25) + 1;
 
