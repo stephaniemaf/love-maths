63dd1c5f4f98c0051b155fe6b99a25a57bea8288
stephaniemaf <farrell.s.ava@gmail.com>
2022-10-26 17:51:14 +0000

    added divison question code for final challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,9 @@ function runGame(gameType){
    else if(gameType === "subtract"){
     displaySubtractQuestion(num1, num2)
    }
+   else if (gameType === "division"){
+    displayDivisionQuestion(num1, num2);
+}
    else{
    alert(`Unknown game type: ${gameType}`);
    throw `Unknown game type: ${gameType}.Aborting!`;
@@ -86,6 +89,9 @@ function calculateCorrectAnswer(){
     else if(operator === "-"){
         return [operand1 - operand2, "subtract"]
     }
+    else if (operator === "/"){
+        return [Math.floor(operand1 / operand2), "division"]
+    }
     else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
@@ -121,6 +127,9 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById("operator").textContent = "x";
 }
 
-function displayDivisionQuestion(){
-    
+function displayDivisionQuestion(operand1, operand2){
+    operand1 = operand1 * operand2;
+	document.getElementById("operand1").textContent = operand1;
+	document.getElementById("operand2").textContent = operand2;
+	document.getElementById("operator").textContent = "/";
 }
